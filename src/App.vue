99b9eca3608fe0ed21100b91d66fcb4adb41e3be
cedric <rakotobembolatiana0@gmail.com>
2024-06-08 09:23:17 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="calculator">
+      <CalculatorDisplay :input="input" :output="cleanOutput" />
+      <CalculatorKeys @keyPressed="handleClick" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorDisplay from './components/CalculatorDisplay.vue';
+import CalculatorKeys from './components/CalculatorKeys.vue';
+import './assets/styles.css'; // Importer le fichier CSS global
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      input: "",
+    };
+  },
+  computed: {
+    cleanOutput() {
+      if (!this.input) return "";
+      try {
+        let result = eval(this.prepareInput(this.input));
+        return result.toLocaleString();
+      } catch (e) {
+        return "";
+      }
+    }
+  },
+  methods: {
+    handleClick(value) {
+      if (value === "clear") {
+        this.input = "";
+      } else if (value === "backspace") {
+        this.input = this.input.slice(0, -1);
+      } else if (value === "=") {
+        this.input = eval(this.prepareInput(this.input)).toString();
+      } else if (value === "brackets") {
+        if (
+          this.input.indexOf("(") === -1 ||
+          (this.input.indexOf("(") !== -1 &&
+            this.input.indexOf(")") !== -1 &&
+            this.input.lastIndexOf("(") < this.input.lastIndexOf(")"))
+        ) {
+          this.input += "(";
+        } else if (
+          this.input.indexOf("(") !== -1 &&
+          this.input.indexOf(")") === -1 ||
+          (this.input.indexOf("(") !== -1 &&
+            this.input.indexOf(")") !== -1 &&
+            this.input.lastIndexOf("(") > this.input.lastIndexOf(")"))
+        ) {
+          this.input += ")";
+        }
+      } else {
+        if (this.validateInput(value)) {
+          this.input += value;
+        }
+      }
+    },
+    prepareInput(input) {
+      return input.replace(/%/g, "/100");
+    },
+    validateInput(value) {
+      const lastInput = this.input.slice(-1);
+      const operators = ["+", "-", "*", "/"];
+      if (value === "." && lastInput === ".") return false;
+      if (operators.includes(value) && operators.includes(lastInput)) return false;
+      return true;
+    }
+  },
   components: {
-    HelloWorld
+    CalculatorDisplay,
+    CalculatorKeys
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CalculatorDisplay.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="display">
+      <div class="content">
+        <div class="input" v-html="cleanInput"></div>
+        <div class="output">{{ output }}</div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['input', 'output'],
+    computed: {
+      cleanInput() {
+        return this.input
+          .split("")
+          .map(char => {
+            if (char === "*") return ` <span class="operator">x</span> `;
+            if (char === "/") return ` <span class="operator">÷</span> `;
+            if (char === "+") return ` <span class="operator">+</span> `;
+            if (char === "-") return ` <span class="operator">-</span> `;
+            if (char === "(") return `<span class="brackets">(</span>`;
+            if (char === ")") return `<span class="brackets">)</span>`;
+            if (char === "%") return `<span class="percent">%</span>`;
+            return char;
+          })
+          .join("");
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .display {
+    min-height: 200px;
+    padding: 1.5rem;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    color: white;
+    text-align: right;
+    flex: 1 1 0%;
+  }
+  
+  .display .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    width: 100%;
+    max-width: 100%;
+    overflow: auto;
+  }
+  
+  .display .input {
+    width: 100%;
+    font-size: 1.25rem;
+    margin-bottom: 0.5rem;
+  }
+  
+  .display .output {
+    font-size: 3rem;
+    font-weight: 700;
+    width: 100%;
+    white-space: nowrap;
+  }
+  
+  .display .operator {
+    color: #eb6666;
+  }
+  
+  .display .brackets,
+  .display .percent {
+    color: #26fed7;
+  }
+  </style>
+  
